668d1adb716fff87748d1967cae9c1738450f8d7
Upasana Nayak <nayakupasana53@gmail.com>
2026-08-12 23:21:43 +0530

    fix cors issue

--- backend/model/db.js
+++ backend/model/db.js
@@ -2,7 +2,7 @@ const mongoose=require("mongoose");
 require("dotenv").config();
 const API=process.env.MONGO_URL;
 async function main() {
-    await mongoose.connect(`${API}/CafeHeaven`);
+    await mongoose.connect(API);
 }
 main().then(()=>{
     console.log("Database is connected");
