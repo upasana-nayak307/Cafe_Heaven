fad2d5b2b6945afcaa2499a7e15dde1f07217b10
Upasana Nayak <nayakupasana53@gmail.com>
2026-08-12 23:11:23 +0530

    fix cors issue

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,7 @@ const authRoutes = require("./routes/authRoutes");
 const allowedOrigins = [
   "http://localhost:5173",
   "http://127.0.0.1:5173",
+  "https://cafe-heaven-red.vercel.app",
   process.env.FRONTEND_URL,
 ].filter(Boolean);
 
